59e5a15560707207d9da297aaa542d506a10dc79
habibahmylah <100356740+habibahmylah@users.noreply.github.com>
2022-03-30 11:51:10 +0700

    Add files via upload

---
+++ NBArray.c
@@ -0,0 +1,29 @@
+// Nama : Habibah Mylah Dalilah
+// NIM : 24060121120028
+// Lab Praktikum : B1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Kamus
+	int i,j,N,NBArray;
+
+    //Algoritma
+    printf("Membuat Program Menghitung Array\n");
+    printf("Masukan nilai N : ");
+    scanf("%d", &N);
+    int T[N];
+    NBArray = 0;
+
+    for (i = 0; i < N; i++) scanf("%d", &T[i]);
+
+    for (i = 0; i < N; i++) {
+        for (j = i; j < N; j++) {
+            NBArray += T[j];
+        }
+    }
+    printf("%d", NBArray);
+    return 0;
+}
